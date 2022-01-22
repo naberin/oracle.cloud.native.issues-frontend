35a3042d0206190a66524a78b6d402081ef23f0c
naberin <aberinnj@gmail.com>
2022-01-22 16:44:55 -0600

    Added DashboardPage

---
+++ src/components/AppActivities/Entry.js
@@ -0,0 +1,8 @@
+
+const component = function (props) {
+
+    return (
+        <div></div>
+    )
+}
+export default component;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,24 @@
 import React from "react";
+import MinifiedProjectListing from "../components/AppProjects/Mini/Listing";
+
 class Component extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            projectListing: []
+        }
+    }
+
     render() {
         return (
-            <section></section>
+            <section className="page flex flex-row">
+                <MinifiedProjectListing
+                    className="flex-grow-3"
+                    listing={this.state.projectListing}
+                />
+
+            </section>
         )
     }
 }
