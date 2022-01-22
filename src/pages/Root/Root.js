1db84daa390e8e0063513124e4e3969eb3f4f58c
naberin <aberinnj@gmail.com>
2022-01-22 16:00:57 -0600

    Added AppHeader and routed DashboardPage in Root

--- src/pages/Root/Root.js
+++ src/pages/Root/Root.js
@@ -1,8 +1,16 @@
 import './Root.css';
+import AppHeader from "../../components/AppHeader/Header";
+import {Route, Routes} from "react-router-dom";
+import Dashboard from "../Dashboard";
 
 function Root() {
   return (
     <main id="app-root" data-testid="app-root">
+            <AppHeader title="Issue Tracker"/>
+
+            <Routes>
+                <Route path="/" element={<Dashboard/>} />
+            </Routes>
     </main>
   );
 }
